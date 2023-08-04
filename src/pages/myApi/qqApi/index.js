5d98d6913dc60f311e34b54cd68f88eece837434
heshiheng <shihengh@chinadci.com>
2023-08-04 18:01:17 +0800

    fix: 天气

--- src/pages/myApi/qqApi/index.js
+++ src/pages/myApi/qqApi/index.js
@@ -89,14 +89,14 @@ const QqApi = (props) => {
   const onSearch = (value) => setValue(value)
   const videocom = <video id='video' src={MusicData.mp3url} width='' height='' controls></video>
 
-   // 自定义比较函数，用于按星期一到星期日的顺序排序
-   const compareDays = (day1, day2) => {
-    const daysOrder = ['星期一', '星期二', '星期三', '星期四', '星期五', '星期六', '星期日'];
-    return daysOrder.indexOf(day1.week) - daysOrder.indexOf(day2.week);
-  };
+  // 自定义比较函数，用于按星期一到星期日的顺序排序
+  const compareDays = (day1, day2) => {
+    const daysOrder = ['星期一', '星期二', '星期三', '星期四', '星期五', '星期六', '星期日']
+    return daysOrder.indexOf(day1.week) - daysOrder.indexOf(day2.week)
+  }
 
   // 按顺序排序数据
-  const sortedData = weatherData.data.slice().sort(compareDays);
+  const sortedData = _.slice(weatherData.data).sort(compareDays)
   return (
     <div className='qq-box'>
       <Row>
@@ -126,17 +126,25 @@ const QqApi = (props) => {
       <Row>
         <Col span={24} style={coloStyle}>
           {weatherData.city}
-          <div className='weather-forecast'>
-            {_.map(sortedData, (day, index) => (
-              <div key={index} className='day-forecast'>
-                <div className='day'>{day.week}</div>
-                <div className='weather'>
-                  <div>{day.type}</div>
-                  <div>{`高温: ${day.high}，低温: ${day.low}`}</div>
-                  <div>{`白天: ${day.fengxiang}${day.fengli}`}</div>
-                  <div>{`夜晚: ${day.night.fengxiang}${day.night.fengli}`}</div>
-                </div>
-              </div>
+          <div className='weather-box'>
+          {_.map(sortedData, (day, index) => (
+              <Card
+                hoverable
+                style={{ width: 240 }}
+                cover={
+                  <div key={index} className='day-forecast'>
+                    <div className='day'>{day.week}</div>
+                    <div className='weather'>
+                      <div>{day.type}</div>
+                      <div>{`高温: ${day.high}，低温: ${day.low}`}</div>
+                      <div>{`白天: ${day.fengxiang}${day.fengli}`}</div>
+                      <div>{`夜晚: ${day.night.fengxiang}${day.night.fengli}`}</div>
+                    </div>
+                  </div>
+                }
+              >
+                <Meta title={weatherData.city} description={weatherData.city} />
+              </Card>
             ))}
           </div>
         </Col>
